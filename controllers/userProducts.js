c17dd770ff05bbe311b3322fab745f591c6971ee
aung_myat <gusion762389@gmail.com>
2025-07-13 18:34:10 +0630

    add style

--- controllers/userProducts.js
+++ controllers/userProducts.js
@@ -13,25 +13,40 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-    // fetch cart
-    req.session.user.populate("cart.products.productId").then((user) => {
-        console.log(user.cart.products);
-        cartProducts = user.cart.products;
-        res.render("shop/cart", {
-            path: "/cart",
-            products: cartProducts,
-            totalPrice: user.cart.totalPrice,
-            isLoggedIn: req.session.isLoggedIn,
-        });
-    });
+    // Instead of req.session.user.populate(), find user by ID first
+    User.findById(req.session.user._id)
+        .populate("cart.products.productId") // populate product details in cart
+        .then((user) => {
+            if (!user) {
+                // handle no user found if needed
+                return res.redirect("/");
+            }
+            const cartProducts = user.cart.products;
+            res.render("shop/cart", {
+                path: "/cart",
+                products: cartProducts,
+                totalPrice: user.cart.totalPrice,
+                isLoggedIn: req.session.isLoggedIn,
+            });
+        })
+        .catch((err) => next(err));
 };
 
 exports.postAddCart = (req, res, next) => {
     const { productId } = req.body;
 
     Product.findById(productId).then((product) => {
-        req.session.user
-            .addToCart(product._id, product.price)
+        // console.log(req.session.user);
+        // req.session.user
+        //     .addToCart(product._id, product.price)
+
+        return User.findById(req.session.user._id)
+            .then((user) => {
+                if (!user) return res.redirect("/login");
+
+                // Now you have full mongoose user doc, call instance method
+                return user.addToCart(productId, product.price);
+            })
             .then((result) => {
                 console.log(result);
                 res.redirect("/cart");
@@ -50,9 +65,19 @@ exports.postAddCart = (req, res, next) => {
 
 exports.postDeleteCart = (req, res, next) => {
     const { productId } = req.body;
-    req.session.user.deleteById(productId).then(() => {
-        res.redirect("/cart");
-    });
+    User.findById(req.session.user._id)
+        .then((user) => {
+            if (!user) {
+                res.redirect("/login");
+            }
+
+            user.deleteById(productId).then(() => {
+                res.redirect("/cart");
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
 
 exports.getOrder = (req, res, next) => {
@@ -66,33 +91,32 @@ exports.getOrder = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-    req.session.user
+    User.findById(req.session.user._id)
         .populate("cart.products.productId")
         .then((user) => {
-            console.log(user.cart.products);
-            cartProducts = user.cart.products;
+            if (!user) {
+                return res.redirect("/login");
+            }
+
+            const cartProducts = user.cart.products;
             const products = cartProducts.map((p) => {
                 return { product: p.productId._doc, qty: p.qty };
             });
-            console.log(products);
+
             const order = new Order({
                 products: products,
                 user: {
-                    name: req.session.user.name,
-                    userId: req.session.user._id,
+                    email: user.email, // or user.name if you have it
+                    userId: user._id,
                 },
             });
-            return order.save();
-        })
-        .then(() => {
-            return req.session.user.clearCart();
+
+            return order.save().then(() => user.clearCart());
         })
         .then(() => {
             res.redirect("/order");
         })
-        .catch((err) => {
-            throw err;
-        });
+        .catch((err) => next(err));
 };
 
 exports.getProductDetail = (req, res, next) => {
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+        "./views/**/*.ejs", // or pug, handlebars, etc.
+        "./public/**/*.html",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
