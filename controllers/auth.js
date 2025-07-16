3ce20fd2ebcf2fd53855d869b11d095d3905b136
aung_myat <gusion762389@gmail.com>
2025-07-16 23:19:57 +0630

    add input validations

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
 
 const User = require("../models/User");
 
@@ -14,32 +15,37 @@ exports.getLogin = (req, res, next) => {
 
 exports.postLogin = (req, res, next) => {
     const { email, password } = req.body;
-    User.findOne({ email: email }).then((user) => {
-        if (!user) {
-            req.flash("error_msg", "Your Email is wrong!");
-            return res.redirect("/login");
-        } else {
-            bcrypt
-                .compare(password, user.password)
-                .then((doMatch) => {
-                    if (!doMatch) {
-                        req.flash("error_msg", "Your Password is wrong!");
-                        return res.redirect("/login");
-                    } else {
-                        req.session.isLoggedIn = true;
-                        req.session.user = user;
-                        return req.session.save((err) => {
-                            console.log(err);
 
-                            res.redirect("/");
-                        });
-                    }
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-        }
-    });
+    const errors = validationResult(req);
+
+    // User.findOne({ email: email }).then((user) => {
+    //     if (!user) {
+    //         req.flash("error_msg", "Your Email is wrong!");
+    //         return res.redirect("/login");
+    //     } else {
+    //         bcrypt
+    //             .compare(password, user.password)
+    //             .then((doMatch) => {
+    //                 if (!doMatch) {
+    //                     req.flash("error_msg", "Your Password is wrong!");
+    //                     return res.redirect("/login");
+    //                 }
+    if (!errors.isEmpty()) {
+        req.flash("error_msg", errors.array()[0].msg);
+        return res.redirect("/login");
+    } else {
+        return req.session.save((err) => {
+            console.log(err);
+
+            res.redirect("/");
+        });
+    }
+    // })
+    // .catch((err) => {
+    //     console.log(err);
+    // });
+    //     }
+    // });
 };
 
 exports.getSignup = (req, res, next) => {
@@ -50,54 +56,57 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postSignup = (req, res, next) => {
-    const { email, password, confirmedPassword } = req.body;
+    const { email, password } = req.body;
 
-    User.findOne({ email: email })
-        .then((userDoc) => {
-            if (userDoc) {
-                req.flash(
-                    "error_msg",
-                    "This email have been used for sign up!"
-                );
-                return res.redirect("/signup");
-            }
-            if (password.length < 6) {
-                req.flash(
-                    "error_msg",
-                    "Your password must be at least 6 characters!"
-                );
-                return res.redirect("/signup");
-            } else if (password !== confirmedPassword) {
-                req.flash("error_msg", "Your passwords must be equal!");
-                return res.redirect("/signup");
-            } else {
-                return bcrypt.hash(password, 12).then((hashPsw) => {
-                    const user = new User({
-                        email: email,
-                        password: hashPsw,
-                        cart: { products: [], totalPrice: 0 },
-                    });
+    let errors = validationResult(req);
+    console.log(errors.array());
+    if (!errors.isEmpty()) {
+        req.flash("error_msg", errors.array()[0].msg);
+        return res.redirect("/signup");
+    }
 
-                    return user.save().then((result) => {
-                        res.redirect("/login");
-                        sendMail(
-                            email,
-                            "Account Signup",
-                            "Account Created",
-                            "<p>You're account have been created!</p>"
-                        );
-                    });
-                });
-            }
-        })
-        .catch((err) => {
-            console.log(err);
+    // User.findOne({ email: email })
+    //     .then((userDoc) => {
+    //         if (userDoc) {
+    //             req.flash(
+    //                 "error_msg",
+    //                 "This email have been used for sign up!"
+    //             );
+    //             return res.redirect("/signup");
+    //         }
+    //         if (password.length < 6) {
+    //             req.flash(
+    //                 "error_msg",
+    //                 "Your password must be at least 6 characters!"
+    //             );
+    //             return res.redirect("/signup");
+    //         } else if (password !== confirmedPassword) {
+    //             req.flash("error_msg", "Your passwords must be equal!");
+    //             return res.redirect("/signup");
+    //         } else {
+    return bcrypt.hash(password, 12).then((hashPsw) => {
+        const user = new User({
+            email: email,
+            password: hashPsw,
+            cart: { products: [], totalPrice: 0 },
         });
+
+        return user.save().then((result) => {
+            res.redirect("/login");
+        });
+    });
 };
+// })
+// .catch((err) => {
+//     console.log(err);
+// });
+// };
 
 exports.postLogout = (req, res, next) => {
-    req.session.destroy((err) => {
-        console.log(err);
-        res.redirect("/");
-    });
+    if (confirm("Are you sure to logout?")) {
+        req.session.destroy((err) => {
+            console.log(err);
+            res.redirect("/");
+        });
+    }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,87 @@
 const express = require("express");
+const { body, check } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 const authControlller = require("../controllers/auth");
+const User = require("../models/User");
 
 const router = express.Router();
 
 router.get("/login", authControlller.getLogin);
 
-router.post("/login", authControlller.postLogin);
+router.post(
+    "/login",
+    [
+        body("email")
+            .isEmail()
+            .withMessage("Your email is incorrect")
+            .custom((value, { req }) => {
+                return User.findOne({ email: value }).then((user) => {
+                    if (!user) {
+                        return Promise.reject(
+                            "Your email hasn't signed up yet"
+                        );
+                    }
+                });
+            }),
+        body("password", "Your password is incorrect").custom(
+            (value, { req }) => {
+                return User.findOne({ email: req.body.email }).then((user) => {
+                    if (!user) {
+                        return Promise.reject(
+                            "No account found with this email"
+                        );
+                    }
+
+                    return bcrypt
+                        .compare(value, user.password)
+                        .then((doMatch) => {
+                            if (!doMatch) {
+                                return Promise.reject(
+                                    "Your password is incorrect"
+                                );
+                            }
+
+                            req.session.isLoggedIn = true;
+                            req.session.user = user;
+                        });
+                });
+            }
+        ),
+    ],
+    authControlller.postLogin
+);
 
 router.get("/signup", authControlller.getSignup);
 
-router.post("/signup", authControlller.postSignup);
+router.post(
+    "/signup",
+    [
+        check("email")
+            .isEmail()
+            .withMessage("Your email is incorrect")
+            .custom((value, { req }) => {
+                return User.findOne({ email: value }).then((user) => {
+                    if (user) {
+                        return Promise.reject(
+                            "Your email had been used for sign up"
+                        );
+                    }
+                });
+            }),
+        body("password")
+            .isLength({ min: 6 })
+            .withMessage("Your password must be at least 6 characters long"),
+        body("confirmedPassword").custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error("Your passwords must be match");
+            }
+
+            return true;
+        }),
+    ],
+    authControlller.postSignup
+);
 
 router.post("/logout", authControlller.postLogout);
 
